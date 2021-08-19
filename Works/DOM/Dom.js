7d6fbda1023f5a3ceea82ed2e3954b9321a991db
FeridPiriyev <smdpiriyev@gmail.com@>
2021-08-19 20:55:45 +0400

    Dom

---
+++ Works/DOM/Dom.js
@@ -0,0 +1,10 @@
+//Crate
+//h1
+
+//let heading=document.createElement("h1")
+
+//heading.innerHTML="Giriş"
+
+//document.body.appendChild(heading)
+
+//document.querySelector("h1").style.border="5px solid blue"-border verir
