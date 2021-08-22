b7b731fa54ed22a7d06aaf63013556912c925602
FeridPiriyev <smdpiriyev@gmail.com@>
2021-08-22 12:42:59 +0400

    Problem Solving

--- Works/Problem Solving/Problem.js
+++ Works/Problem Solving/Problem.js
@@ -1,3 +1,12 @@
 function openModal(){
     document.querySelector('.ks-overlay').style.display='block'
+    document.querySelector('.ks-modal-content').style.top='50%'
+}
+
+function hideModal(){
+    document.querySelector('.ks-overlay').style.display='none'
+}
+
+function findElement(e){
+    console.log(e.target)
 }
